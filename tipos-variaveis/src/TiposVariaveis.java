f7d72e48625c74487389cefb89b386ce5170b4d3
ryanmeneses <ryanmeneses.dev@gmail.com>
2024-05-22 10:49:56 -0300

    Adição de uma programa simples para implementação de tipos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
+
+import java.util.Scanner;
+
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        int valueReceived;
+        try(Scanner input = new Scanner(System.in)) {
+            System.out.println("Type a number between 1 and 10: ");
+            valueReceived = input.nextInt();
+            if (valueReceived >= 1 && valueReceived <=10){
+                System.out.println("OK");
+            }
+            else{
+                System.out.println("Not OK");
+            }
+        }
     }
 }
